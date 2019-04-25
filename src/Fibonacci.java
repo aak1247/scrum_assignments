84ce57244d8845977da23e97c9816c301360ae65
aak1247 <aak1247@126.com>
2019-04-25 14:42:41 +0800

    feat: static import out

--- src/Fibonacci.java
+++ src/Fibonacci.java
@@ -3,6 +3,8 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import static java.lang.System.out;
+
 class Fibonacci {
     private static List<Integer> range(int min, int max) {
         return Stream.iterate(min, i -> i + 1).limit(max - min + 1).collect(Collectors.toList());
@@ -10,7 +12,8 @@ class Fibonacci {
     private static List<Integer> range(int max) {
         return range(0, max);
     }
-    public static BigInteger of(int input){
+
+    private static BigInteger of(int input) {
         if (input <= 2) return BigInteger.valueOf(1);
         var res = BigInteger.valueOf(1);
         var last = BigInteger.valueOf(1);
@@ -22,14 +25,14 @@ class Fibonacci {
         return res;
     }
 
-    public static BigInteger of_r(int input){
+    private static BigInteger of_r(int input) {
         if (input <= 2) return BigInteger.valueOf(1);
         else return of_r(input - 1).add(of_r(input - 2));
     }
 
     public static void main(String args[]) {
         for (var i : range(200)){
-            System.out.println("Fibonacci.of(" + i +") == " + of(i));
+            out.println("Fibonacci.of(" + i + ") == " + of(i));
         }
     }
 }
