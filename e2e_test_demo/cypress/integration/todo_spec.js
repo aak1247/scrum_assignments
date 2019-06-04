2cc93a7e96e2861ce1efb2ec8267fd9967e7ac91
aak1247 <aak1247@126.com>
2019-06-04 19:16:21 +0100

    feat: enable travis for e2e test demo

--- e2e_test_demo/cypress/integration/todo_spec.js
+++ e2e_test_demo/cypress/integration/todo_spec.js
@@ -1,13 +1,45 @@
-//import { describe, it, expect, cy } from 'cypress'
+/// <reference types="Cypress" />
 
-describe('My First Test', function () {
-  it('Test of test', function () {
-    expect(true).to.equal(true)
+
+describe('Visit Test', () => {
+  it('successfully loads', () => {
+    cy.visit('http://localhost:8080')
   })
 })
 
-describe('Visit Test', function () {
-  it('successfully loads', function () {
-    cy.visit('/')
+describe('Add Test', () => {
+  it('successfully added a task', () => {
+    cy.visit('http://localhost:8080')
+    cy.get('p').contains('Create your first todo! :)')
+    cy.get('input').type('task 1')
+    cy.get('.create-todo-form > button').click()
+    cy.get('.todo-not-completed').should('have.length',1)
+    cy.get('.todo-not-completed').contains('task 1')
+  })
+})
+
+describe('Delete Test', () => {
+  it('successfully add a task and remove it', () => {
+    cy.visit('http://localhost:8080')
+    cy.get('p').contains('Create your first todo! :)')
+    cy.get('input').type('task 1')
+    cy.get('.create-todo-form > button').click()
+    cy.get('.todo-not-completed').should('have.length',1)
+    cy.get('.todo-not-completed').contains('task 1').get('button.delete-btn').click()
+    cy.get('p').contains('Create your first todo! :)')
+  })
+})
+
+describe('Edit Test', () => {
+  it('successfully add a task and remove it', () => {
+    cy.visit('http://localhost:8080')
+    cy.get('p').contains('Create your first todo! :)')
+    cy.get('input').type('task 1')
+    cy.get('.create-todo-form > button').click()
+    cy.get('.todo-not-completed').should('have.length',1)
+    cy.get('tr.todo-not-completed').contains('task 1').get('button').contains('Edit').click()
+    cy.get('.todo-not-completed input').clear().type('task 2')
+    cy.get('.todo-not-completed button').contains('Save').click()
+    cy.get('tr.todo-not-completed').contains('task 2')
   })
 })
