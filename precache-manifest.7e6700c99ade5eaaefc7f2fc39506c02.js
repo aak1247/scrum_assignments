ade0133672b23499173447b1766265fb74115a0e
aak1247 <aak1247@126.com>
2019-06-19 10:12:29 +0000

    built with travis:JOB 34.6

---
+++ precache-manifest.7e6700c99ade5eaaefc7f2fc39506c02.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d5de3874d925da5c1f41",
+    "url": "/scrum_assignments/static/js/runtime~main.d5de3874.js"
+  },
+  {
+    "revision": "748a8fcd8745a692f4cb",
+    "url": "/scrum_assignments/static/js/main.748a8fcd.chunk.js"
+  },
+  {
+    "revision": "f6860dc707a8ea4b348c",
+    "url": "/scrum_assignments/static/js/1.f6860dc7.chunk.js"
+  },
+  {
+    "revision": "748a8fcd8745a692f4cb",
+    "url": "/scrum_assignments/static/css/main.6f390579.chunk.css"
+  },
+  {
+    "revision": "f6860dc707a8ea4b348c",
+    "url": "/scrum_assignments/static/css/1.41581fb6.chunk.css"
+  },
+  {
+    "revision": "58eeba15c3f5621d12b0506105bde9ff",
+    "url": "/scrum_assignments/index.html"
+  }
+];
